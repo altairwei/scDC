457ac6515c65043d8d17daddf3eab819165dc08c
Altair Wei <altair_wei@outlook.com>
2021-11-27 14:50:29 +0800

    remote duplicated calculation within fitGLM

--- R/fitGLM.R
+++ R/fitGLM.R
@@ -35,69 +35,69 @@ fitGLM <- function(res, condition, subject_effect = TRUE, pairwise = TRUE, fixed
 
   fit_random <- list()
   fit_fixed <- list()
+
   for(i in 1:ncol(res$nstar)){
     # idx <- indexes_list[[i]]
-    if(verbose){
-      if(i%%10==0){
+    if (verbose) {
+      if(i %% 10 == 0){
         print(paste("fitting GLM...", i))
       }
     }
-
 
     glm_df <-  cbind(res$info[,1:2], res$nstar[,i])
 
     # glm_df <- melt(glm_df)
     colnames(glm_df) <- c("cellTypes", "subject", "cell_count")
     glm_df$cond <- condition
- 
-    mod <- stats::glm(cell_count ~ cellTypes   + cond +  cellTypes:cond + subject,
-                      data = glm_df,
-                      family = poisson(link=log))
 
+    mod <- stats::glm(
+      cell_count ~ cellTypes + cond + cellTypes:cond + subject,
+      data = glm_df, family = poisson(link=log)
+    )
 
     degree_freedom = mod$df.null
 
-    if(subject_effect){
+    if (subject_effect) {
+      # No difference between `pairwise=TRUE/FALSE`
       if(pairwise){
+        fit_fixed[[i]] <- mod
 
-        fit_fixed[[i]] <- stats::glm(cell_count ~ cellTypes + cond +  cellTypes:cond + subject,
-                                     data = glm_df,
-                                     family = poisson(link=log))
         if(!fixed_only){
-          fit_random[[i]] <- lme4::glmer(cell_count ~ cellTypes + cond +  cellTypes:cond + (1 | subject ),
-                                         data = glm_df, family = poisson(link=log),
-                                         control = glmerControl(nAGQ = 0L))
+          fit_random[[i]] <- lme4::glmer(
+            cell_count ~ cellTypes + cond + cellTypes:cond + (1 | subject ),
+            data = glm_df, family = poisson(link=log), control = glmerControl(nAGQ = 0L)
+          )
         }
-      }else{
-        fit_fixed[[i]] <- stats::glm(cell_count ~ cellTypes + cond +  cellTypes:cond + subject,
-                                     data = glm_df, family = poisson(link=log))
-        if(!fixed_only){
+      } else {
+        fit_fixed[[i]] <- mod
 
-          fit_random[[i]] <- lme4::glmer(cell_count ~ cellTypes + cond +
-                                           cellTypes:cond + (1 | subject ), data = glm_df,
-                                         family = poisson(link=log),
-                                         control = glmerControl(nAGQ = 0L))
+        if(!fixed_only){
+          fit_random[[i]] <- lme4::glmer(
+            cell_count ~ cellTypes + cond + cellTypes:cond + (1 | subject ),
+            data = glm_df, family = poisson(link=log), control = glmerControl(nAGQ = 0L)
+          )
         }
       }
-    }else{
-      fit_fixed[[i]] <- stats::glm(cell_count ~ cellTypes + cond +  cellTypes:cond, data = glm_df,
-                                   family = poisson(link=log))
+    } else {
+      fit_fixed[[i]] <- stats::glm(
+        cell_count ~ cellTypes + cond +  cellTypes:cond,
+        data = glm_df, family = poisson(link=log)
+      )
     }
-
   }
 
   if(!subject_effect){
     fixed_only = TRUE
   }
 
-  if(!fixed_only){
+  if (!fixed_only) {
     pool_res_random = mice::pool(fit_random,  dfcom = degree_freedom)
     pool_res_fixed = mice::pool(fit_fixed ,  dfcom = degree_freedom)
     return(list(pool_res_random = pool_res_random,
                 pool_res_fixed = pool_res_fixed,
                 fit_random = fit_random,
                 fit_fixed = fit_fixed))
-  }else{
+  } else {
     pool_res_fixed = mice::pool(fit_fixed,  dfcom = degree_freedom)
     return(list(pool_res_fixed = pool_res_fixed,
                 fit_fixed = fit_fixed))
